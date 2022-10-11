13e3299a065a6bf5acd067bb92c571b9eff0a443
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-10-11 10:31:18 +0900

    [unrated] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.js
@@ -1,8 +1,10 @@
 function solution(n) {
     var answer = [];
-    let numbers = Array(n).fill(0).map((_,i)=>i+1)
-    //console.log(numbers)
-    answer = [...numbers.filter(i=>n%i==0)]
-    
+    for(let i =0;i<n+1;i++){
+        if (n%i==0){
+            answer.push(i)
+        }
+    }
     return answer;
 }
+
