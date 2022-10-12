8a1b8641f50a2adc0a3553bcbbe50f71ac956306
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-10-12 22:41:56 +0900

    [unrated] Title: 순서쌍의 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,14 +1,14 @@
 function solution(n) {
     var answer = 0;
     for (let i = 0 ; i < n**0.5 ; i++){
-        if (n%i==0){
-            answer+=1;
+        if (n%i == 0){
+            answer += 1;
         }
     }
     answer*=2;
     
-    if((n**0.5)%1==0){
-        answer+=1
+    if ((n**0.5) %1 ==0){
+        answer += 1
     }
     
     return answer;
