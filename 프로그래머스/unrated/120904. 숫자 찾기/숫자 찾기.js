de8b8393bc2be193d8f4a150f9421a71a5beaa9f
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-10-12 22:48:19 +0900

    [unrated] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
@@ -1,8 +1,7 @@
 function solution(num, k) {
     var answer = 0;
     
-    let tmp = [...String(num)].map(x=>+x)
-    answer = tmp.indexOf(k)
+    answer = num.toString().indexOf(k.toString())
     
     if (answer != -1){
         answer +=1 
