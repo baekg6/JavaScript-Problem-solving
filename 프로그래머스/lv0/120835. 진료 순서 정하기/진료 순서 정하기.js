bfd8d401974eec8d9c5af996417f1df1bab62bbb
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-11-29 09:52:28 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,6 +1,5 @@
 function solution(emergency) {
     const sortedArr = [...emergency].sort((a,b)=>b-a);
-    console.log(sortedArr)
     let answer = []
     emergency.forEach(el=>{
         answer.push(sortedArr.indexOf(el)+1)
