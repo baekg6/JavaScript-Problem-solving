ee837c09ef72b10ca26f784592b0e5bbe189c570
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-12-01 11:11:39 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    let dupCheck = []
-    
-    for (const i of my_string){
-        if (dupCheck.indexOf(i)===-1){
-            answer+=i
-            dupCheck.push(i)
-        }
-    }
-    return answer;
+    return [...new Set(my_string)].join('');
 }
